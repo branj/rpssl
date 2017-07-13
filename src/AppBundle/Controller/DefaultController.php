b6c9fe1790cd931442e628f8b67b674657fc604f
B Money <brandonleejennings@gmail.com>
2017-07-13 00:47:17 -0700

    Added a little ui improvements and some comments

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use AppBundle\Form\GameType;
 use AppBundle\Entity\Game;
 use AppBundle\Entity\GameState;
@@ -13,28 +14,67 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class DefaultController extends Controller
 {
     /**
+     * @var string The index used to house previous games for this visitor
+     */
+    const SESSION_INDEX = 'previousGames';
+
+    /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
+        //Set up the Game form
         $game = new Game();
         $form = $this->createForm(GameType::class, $game);
         $form->handleRequest($request);
 
+        //Determine if they had previous games.
+        $session = $request->getSession();
+        $previousGames = $session->get('previousGames', []);
+
+        //See if they decided to play game, if so, ensure it passes validation.
         if ($form->isSubmitted() && $form->isValid()) {
             $game = $form->getData();
             $em = $this->getDoctrine()->getManager();
+
+            /**
+             * Saving the $game triggers the PlayGame listener, which will execute game logic.
+             * @see  AppBundle\EventListener\PlayGame
+             */
             $em->persist($game);
             $em->flush();
             
-            $this->addFlash(
-                'success',
-                'Your changes were saved!'
-            );
+            //The state of the game will tell us who won.
+            $state = $game->getState();
+
+            //Same the game to the user's session so they can browse statistics.
+            $previousGames[] = $game->getId();
+            $session->set(self::SESSION_INDEX, $previousGames);
+
+            //Let's set up the message back to the user.
+            $message = $game->getOpponentDecision()->getName(). " vs. ". $game->getComputerDecision()->getName() . "\n";
+            $status = "";
+            $flashClass = "";
+            if ($state->isAWin()) {
+                $message .= "You Won!";
+                $flashClass = "success";
+            } elseif ($state->isALoss()) {
+                $message .= "You Lost :(";
+                $flashClass = "warning";
+            } elseif ($state->isADraw()) {
+                $message .= "Draw";
+                $flashClass = "notice";
+            } else {
+                $message .= "Ooops!? Something bad happened...";
+                $flashClass = "notice";
+            }
+
+            $this->addFlash($flashClass, $message);
         }
 
         return $this->render('default/index.html.twig', [
             'form' => $form->createView(),
+            'games' => $previousGames
         ]);
     }
 }
